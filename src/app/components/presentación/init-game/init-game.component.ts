2f834b1dd43340adb01649bc89121447405eeb33
Aydee-qm <aydee.qm@gmail.com>
2019-06-01 15:00:18 -0500

    update presentation

--- src/app/components/presentación/init-game/init-game.component.ts
+++ src/app/components/presentación/init-game/init-game.component.ts
@@ -7,11 +7,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./init-game.component.scss']
 })
 export class InitGameComponent implements OnInit {
+  public isLoading = true;
 
-  constructor(private _router: Router) { }
+  constructor(private _router: Router) {
+    
+   }
 
   ngOnInit() {
+    this.handleLoading();
   }
+
+  handleLoading() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  }
+
   navegar(idNivel){
     this._router.navigate(['/linea-de-juego']);
   }
