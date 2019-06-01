4ab013cb50bd413473a17d4105a5d5c3a29c9c22
yulissateran <yulissa.lteran@gmail.com>
2019-06-01 10:48:13 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FaceComponent } from './components/juegos/face/face.component';
+import { LecturaComponent } from './components/lectura/lectura.component';
+import { ReflexionComponent } from './components/reflexion/reflexion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cuento-1',
+    pathMatch: 'full',
+    component: LecturaComponent
+},
+{
+    path: 'faces',
+    pathMatch: 'full',
+    component: FaceComponent
+},
+{
+  path: 'reflexion-1',
+  pathMatch: 'full',
+  component: ReflexionComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
