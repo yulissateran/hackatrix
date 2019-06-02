4a30b256057b7f6862206628307e4e81de7b349e
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2019-06-01 19:39:41 -0500

    ultimo cambio

--- src/app/components/juegos/face/face.component.ts
+++ src/app/components/juegos/face/face.component.ts
@@ -14,44 +14,45 @@ export class FaceComponent implements OnInit {
   public curretIndexQuestion: number = 0;
   public question: any = {};
   public progress: number = 0;
-  constructor(private Location: Location,private stateService: StateService,  private _router: Router) {
+  constructor(private Location: Location, private stateService: StateService, private _router: Router) {
     this.state = this.stateService.levels[0].game;
     this.question = this.state.questions[this.curretIndexQuestion];
-    
+
   }
 
   ngOnInit() {
   }
-  irAReflexion(){
-  this._router.navigate(['/reflexion-1'])
+  irAReflexion() {
+    this._router.navigate(['/reflexion-1'])
   }
- reset(){
-  this.state = this.stateService.levels[0].game;
-  this.question = this.state.questions[this.curretIndexQuestion];
-  this.state = {};
-  this.curretIndexQuestion = 0;
-  this.question = {};
-  this.progress = 0;
- }
+  reset() {
+    // this.state = this.stateService.levels[0].game;
+    this.question = this.state.questions[this.curretIndexQuestion];
+    this.state = {};
+    this.curretIndexQuestion = 0;
+    this.question = {};
+    this.progress = 0;
+  }
 
- atras() {
-  this.Location.back();
-}
+  atras() {
+    this.Location.back();
+  }
 
   select(response) {
-    
-      if (this.question.response === response) {
-        console.log(this.question.response , 'correct response:', response);    
-      console.log(this.curretIndexQuestion )
-        if (this.curretIndexQuestion === 2) {
-          setTimeout(()=>this.irAReflexion(), 1000);
-          console.log('fin');
-        } else {
-          this.curretIndexQuestion = this.curretIndexQuestion + 1;
-          this.question = {};
-          this.question = this.state.questions[this.curretIndexQuestion];
-          this.progress = this.progress + 1;
-        }
+
+    if (this.question.response === response) {
+      console.log(this.question.response, 'correct response:', response);
+      console.log(this.curretIndexQuestion)
+      if (this.curretIndexQuestion === 2) {
+        this.progress = this.progress + 1;
+        setTimeout(() => this.irAReflexion(), 1500);
+        console.log('fin');
+      } else {
+        this.curretIndexQuestion = this.curretIndexQuestion + 1;
+        this.question = {};
+        this.question = this.state.questions[this.curretIndexQuestion];
+        this.progress = this.progress + 1;
+      }
     }
 
   }
