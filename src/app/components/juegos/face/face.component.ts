fb632e795fc29adca05b173d0c25a899552fd5d1
Aydee-qm <aydee.qm@gmail.com>
2019-06-01 19:27:08 -0500

    -

--- src/app/components/juegos/face/face.component.ts
+++ src/app/components/juegos/face/face.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { StateService } from 'src/app/services/state.service';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { log } from 'util';
 
 @Component({
   selector: 'app-face',
@@ -38,21 +39,19 @@ export class FaceComponent implements OnInit {
 }
 
   select(response) {
-    if (this.curretIndexQuestion === 2) {
-      this.progress = this.progress + 1;
-      setTimeout(()=>this.irAReflexion(), 1000);
-      
-      console.log('fin');
-    } else {
+    
       if (this.question.response === response) {
-        console.log(this.question.response , response);    
-        this.curretIndexQuestion = this.curretIndexQuestion + 1;
-        this.question = {};
-        this.question = this.state.questions[this.curretIndexQuestion];
-        this.progress = this.progress + 1;
-      } else {
-  
-      }
+        console.log(this.question.response , 'correct response:', response);    
+      console.log(this.curretIndexQuestion )
+        if (this.curretIndexQuestion === 2) {
+          setTimeout(()=>this.irAReflexion(), 1000);
+          console.log('fin');
+        } else {
+          this.curretIndexQuestion = this.curretIndexQuestion + 1;
+          this.question = {};
+          this.question = this.state.questions[this.curretIndexQuestion];
+          this.progress = this.progress + 1;
+        }
     }
 
   }
